02dd23c500fffc80a033dd7c892f10f2160f9ca5
Nicolas BOURGAREL <nbourgarel@gmail.com>
2024-05-03 13:22:54 +0200

    feature: added universal mousetracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'ng_enhilion';
+  title = "ng_enhilion";
+
+  mouseX: number = 0;
+  mouseY: number = 0;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  @HostListener("document:mousemove", ["$event"])
+  onMouseMove(event: MouseEvent) {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  }
 }
