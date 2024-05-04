11e4cc229c6b3d5b48f367255145d074b8959787
Nicolas BOURGAREL <nbourgarel@gmail.com>
2024-05-04 14:20:25 +0200

    feature: card page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomePage } from "./pages/home/home.page";
+import { CardComponent } from "./components/card/card.component";
 
 const routes: Routes = [
   { path: "home", component: HomePage },
+  { path: "card", component: CardComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePage } from './pages/home/home.page';
 import { StarfieldComponent } from './components/starfield/starfield.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePage,
-    StarfieldComponent
+    StarfieldComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
