59c1e885e245dd70504dd66348e8ee011300031a
Nicolas BOURGAREL <nbourgarel@gmail.com>
2024-05-04 17:54:44 +0200

    Revert "perf: update to angular 17, update cursor behaviour"
    This reverts commit a2b9bf3f096eb441dc760957c3db0f606ca50abd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HomePage } from "./pages/home/home.page";
-import { StarfieldComponent } from "./components/starfield/starfield.component";
-import { CardComponent } from "./components/card/card.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePage } from './pages/home/home.page';
+import { StarfieldComponent } from './components/starfield/starfield.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePage, StarfieldComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomePage,
+    StarfieldComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
